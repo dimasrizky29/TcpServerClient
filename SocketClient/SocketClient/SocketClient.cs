a61fe5fbb0f177c213c54bc20fe35ef51ff7ad1a
dimasrizky29 <63985999+dimasrizky29@users.noreply.github.com>
2021-07-10 23:27:15 +0700

    Add files via upload

---
+++ SocketClient/SocketClient/SocketClient.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace socket_client
+{
+    public class ReadMessage
+    {
+        public void Read(object argument)
+        {
+            TcpClient tcpClient = (TcpClient)argument;
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+
+            while (true)
+            {
+                try
+                {
+                    string message = reader.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+        }
+    }
+    class SocketClient
+    {
+        public static void Main()
+        {
+            ReadMessage read = new ReadMessage();
+            try
+            {
+                Console.WriteLine(" Client Started ");
+                TcpClient clientSocket = new TcpClient();
+                clientSocket.Connect("127.0.0.1", 1000);
+                Console.WriteLine(" Server Connected ");
+
+                Thread thread = new Thread(read.Read);
+                thread.Start(clientSocket);
+
+                StreamWriter writer = new StreamWriter(clientSocket.GetStream());
+                
+                Console.WriteLine("Input Username");
+                string name = Console.ReadLine();
+                writer.WriteLine(name);
+
+                Console.WriteLine("Input Message");
+                while (true)
+                {
+                    if (clientSocket.Connected)
+                    {
+                        //send message 
+                        string input = Console.ReadLine();
+                        writer.WriteLine(input);
+                        writer.Flush();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+            Console.WriteLine("Press any key to exit from client program");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SocketServer/SocketServer/SocketServer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Linq;
+
+namespace socket_server
+{
+    class SocketServer
+    {
+        static TcpListener Listener;
+        static TcpClient tcpClients;
+        static List<TcpClient> clientsList = new List<TcpClient>();
+        private static List<string> messages = new List<string>();
+        static Int32 port = 1000;
+        static IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+
+        public static void ClientListener(object argument, int num)
+        {
+            TcpClient tcpClient = (TcpClient)argument;
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+
+            Console.WriteLine("Client " + num.ToString() + " connected...");
+
+            try
+            {
+                string name = reader.ReadLine();
+
+                while (true)
+                {
+                    string message = reader.ReadLine();
+                    BroadCast(message, name, tcpClient, num);
+                    string chat = name + " : " + message;
+                    Console.WriteLine(chat);
+
+                    writeMessage(chat);
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine(" ");
+            }
+            finally
+            {
+                clientsList.Remove(tcpClient);
+                Console.WriteLine("Client " + num.ToString() + " Disconnected...");
+
+                if (clientsList?.Any() == false)
+                {
+                    tcpClients.Client.Disconnect(true);
+                    tcpClients.Client.Close();
+                    Console.WriteLine("Server Closed...");
+                    System.Environment.Exit(1);
+                }
+            }
+
+        }
+
+        public static void BroadCast(string msg, string nam, TcpClient currentClient, int n)
+        {
+            foreach (TcpClient client in SocketServer.clientsList)
+            {
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                if (client != currentClient)
+                {
+                    writer.WriteLine(nam + " : " + msg);
+                }
+                writer.Flush();
+            }
+        }
+
+        public static void Main()
+        {
+            Listener = new TcpListener(localAddress, port);
+            Listener.Start();
+            Console.WriteLine("Server created...");
+            int clientCounter = 0;
+
+            try
+            {
+                while (true)
+                {
+                    tcpClients = Listener.AcceptTcpClient();
+                    clientsList.Add(tcpClients);
+                    Console.WriteLine(" New Client Accepted...!");
+
+                    //start listener
+                    Thread clientThread = new Thread(() => ClientListener(tcpClients, clientCounter));
+                    clientThread.Start();
+
+                    clientCounter++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static void writeMessage(string chat)
+        {
+            messages.Add(chat);
+            File.WriteAllLines("D:/jaringan komputer/tugas/chat.txt", messages);
+        }
+    }
+}
